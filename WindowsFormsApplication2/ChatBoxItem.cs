c5d787c800070c0dae5da8184e4b2b034580fc32
Jkumamot <jonathan@kumamoto.us>
2017-02-25 01:54:11 -0800

    deleted all old files and setup the project the correct way with updates to the bot

---
+++ WindowsFormsApplication2/ChatBoxItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+public class ChatBoxItem
+{
+    public ChatBoxItem(Color c, string m)
+    {
+        ItemColor = c;
+        Message = m;
+    }
+    public Color ItemColor { get; set; }
+    public string Message { get; set; }
+}
