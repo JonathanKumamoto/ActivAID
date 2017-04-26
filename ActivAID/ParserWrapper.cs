9c65b75808e1141716437feeae09a99c80327a33
Matthew Frazer <frazerm@uci.edu>
2017-04-26 00:21:32 -0700

    added xml serialization to parser

--- ActivAID/ParserWrapper.cs
+++ ActivAID/ParserWrapper.cs
@@ -5,15 +5,57 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using ActivAID;
-
-
+using OpenTextSummarizer;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace Parser
 {
+    [XmlRoot(ElementName = "file")]
+    public class FileRegex
+    {
+        [XmlElement(ElementName = "fileName")]
+        public string name { get; set; }
+
+        [XmlElement(ElementName = "pattern")]
+        public string pattern { get; set; }
+
+    }
+
+    [XmlRoot(ElementName = "patterns")]
+    public class RegexList
+    {
+        public RegexList()
+        {
+            filePatternsArray = new List<FileRegex>();
+        }
+        public void Add(FileRegex fgex)
+        {
+            filePatternsArray.Add(fgex);
+        }
+        [XmlArrayItem()]
+        public List<FileRegex> filePatternsArray { set; get; }
+    }
+
     class ParserWrapper
     {
         Dictionary<string, ParsedCHM> parsedCHMs;
         ActivAIDDB db;
+        RegexList fgexes;
+        private void initializeFGEXES()
+        {
+            try
+            {
+                System.IO.StreamReader str = new System.IO.StreamReader(@"config_patterns.xml");
+                System.Xml.Serialization.XmlSerializer xSerializer = new System.Xml.Serialization.XmlSerializer(typeof(RegexList));
+                fgexes = (RegexList)xSerializer.Deserialize(str);
+                str.Close();
+            }
+            catch (Exception ex)
+            {
+                fgexes = new RegexList();
+            }
+        }
 
         public ParserWrapper(string filePath)
         {
@@ -21,7 +63,9 @@ namespace Parser
             db.insertIntoFiles(filePath);
             parsedCHMs = new Dictionary<string, ParsedCHM>();
             parsedCHMs[filePath] = new ParsedCHM(filePath);
+            initializeFGEXES();
             persistData();
+                        
         }
 
         public ParserWrapper(List<string> filePaths)
@@ -32,6 +76,7 @@ namespace Parser
             {
                 parsedCHMs[file] = new ParsedCHM(file);
             }
+            initializeFGEXES();
             persistData();
             Console.WriteLine("Data successfully inserted into database");
         }
@@ -66,6 +111,73 @@ namespace Parser
             }
         }
 
+        private string generateRegexPatterns(List<string> elementData)
+        {
+            Func<string[], string[]> summarize = new Func<string[], string[]>((toSummarize) =>
+            {
+                // Need to change
+                List<string> sumList = new List<string>();
+                OpenTextSummarizer.SummarizerArguments args = new OpenTextSummarizer.SummarizerArguments();
+                args.InputString = String.Join(" ", toSummarize);
+                OpenTextSummarizer.SummarizedDocument sd = OpenTextSummarizer.Summarizer.Summarize(args);
+                return sd.Concepts.Take(4).ToArray();
+            });
+
+            string regexPattern = "";
+            int count = 0;
+            foreach (string str in summarize(elementData.ToArray()))
+            {
+                if (count != 0)
+                {
+                    regexPattern += "|" + str;
+                }
+                ++count;
+            }
+            return regexPattern;
+        }
+
+        private List<string> aggregateElementData(List<List<Element>> blocks)
+        {
+            Func<string, string> message = new Func<string, string>((x) =>
+            {
+                var temp = x;
+                new HTMLMessager().removeFromLine(ref temp);
+                return temp;
+            });
+
+            List<string> elementData = new List<string>();
+            foreach (List<Element> block in blocks)
+            {
+                foreach (Element element in block)
+                {
+                    if (element.isText)
+                    {
+                        elementData.Add(message(element.data));
+                    }
+                }
+            }
+            return elementData;
+        }
+        //tup.Item2.Select((x) => stringOp(x)).ToArray()
+        private void genRegexPerFile(string filePath, List<List<Element>> blocks)
+        {
+            List<string> elementData = aggregateElementData(blocks);
+            string regex = generateRegexPatterns(elementData);
+            FileRegex fgex = new FileRegex();
+            fgex.name = filePath;
+            fgex.pattern = regex;
+            fgexes.Add(fgex);
+
+        }
+        private void insertRegexIntoConfig()
+        {       
+            using (Stream fileStream = new FileStream("config_patterns.xml", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(RegexList));
+                serializer.Serialize(fileStream, fgexes);
+            }
+        }
+
         public void persistData()
         {
             //ActiveAIDDB db = new ActiveAIDDB();
@@ -73,12 +185,14 @@ namespace Parser
 
             foreach (KeyValuePair<string, ParsedCHM> pair in parsedCHMs)
             {
+                getRegexPerFile(pair.Key, pair.Value.blocks);
                 insertBlocksIntoDB(pair.Key, pair.Value.blocks);
                 insertHREFSOIntoDB(pair.Value.title, pair.Value.hrefs);
             }
+            insertRegexIntoConfig();
         }
 
-        public void genModel()
+        /*public void genModel()
         {
             List<List<string>> fileKeyWords = new List<List<string>>();
             List<string> responseFileNames = new List<string>();
@@ -94,11 +208,11 @@ namespace Parser
             response = String.Join(";", responseFileNames.ToArray());
             //aggregateKeyWords(fileKeyWords)
             //Console.WriteLine(response);
-            /*foreach (string keyword in keywords)
+            foreach (string keyword in keywords)
             {
                 Console.WriteLine(keyword);
-            }*/
-        }
+            }
+        }*/
     }
 }
 
