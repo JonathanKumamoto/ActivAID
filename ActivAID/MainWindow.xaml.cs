9ef4f7cfc9c1979d77a855ecf56448491d7c64f1
Jkumamot <jonathan@kumamoto.us>
2017-05-01 12:48:52 -0700

    added

--- ActivAID/MainWindow.xaml.cs
+++ ActivAID/MainWindow.xaml.cs
@@ -108,6 +108,7 @@ namespace ActivAID
 
                 mic.IsEnabled = true;
                 sRecognize.RecognizeAsyncStop();
+                sRecognize.Dispose();
 
                 
             }
